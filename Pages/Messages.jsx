e48c0bf6bb047affe5162e830689e36b14e59c1f
Roland Edney <redney@maets.net>
2025-09-13 14:44:31 -0400

    Possible "final" product ?

--- Pages/Messages.jsx
+++ Pages/Messages.jsx
@@ -11,7 +11,8 @@ function Messages() {
   return (
     <>
         <div id='messagesMainContent'>
-            <input type='text' id='messagesSearch' placeholder='Search..' title='Search for a forum'/>
+            {/* <input type='text' id='messagesSearch' placeholder='Search..' title='Search for a forum'/> */}
+            <h1>Coming soon...</h1>
         </div>
     </>
   )
--- Pages/Solutions.jsx
+++ Pages/Solutions.jsx
@@ -14,48 +14,100 @@ function Solutions() {
   const [solutions, setSolutions] = useState([])
   const [category, setCategory] = useState('')
   const [categories, setCategories] = useState([])
+  const [elementStatus, setElementStatus]= useState(false)
+  const [filteredItems, setFilteredItems] = useState ([])
    useEffect(  ()=> {
     const getSolutions = async () => {
       const response = await axios.get("http://localhost:8080/solutions")
       setSolutions(response.data)
-       const alreadyThere = new Set();
-    const newArr = [];
-    for (let i = 0; i < solutions.length; i++) {
+      const alreadyThere = new Set();
+      const newArr = [];
+      for (let i = 0; i < solutions.length; i++) {
       const item = solutions[i];
-      if(!alreadyThere.has(item.category)) {
-        alreadyThere.add(item.category)
-        newArr.push(item);
+        if(!alreadyThere.has(item.category)) {
+          alreadyThere.add(item.category)
+          newArr.push(item);
+        }
       }
-    }
-    setCategories(newArr)
+      setCategories(newArr)
+      if (!category) {
+        return
+      } else {
+        const newFilteredArray = []
+        for (let i = 0; i < solutions.length; i++) {
+          const item = solutions[i]
+          if(item.category === category) {
+            newFilteredArray.push(item)
+          }
+        }
+        setFilteredItems(newFilteredArray)
+      }
     }
     getSolutions()
    },[categories])
-
+   
    
     return (
             <div id='solutionsMainDiv'>
-              <div id='wrapper'>
+              <div className='wrapper'>
               <button id='selectCategoryButton' type='button' onClick={()=> {
                 const categoriesElement = document.querySelector('.categoriesDiv')
-                if (categoriesElement) {
+                const wrapperElement = document.querySelector('.wrapper')
+                if (categoriesElement && wrapperElement && elementStatus === false) {
                   categoriesElement.classList.add('show')
+                  wrapperElement.classList.add('show')
+                  setElementStatus(true)
+                }
+                if (categoriesElement && wrapperElement && elementStatus === true) {
+                  categoriesElement.classList.remove('show')
+                  wrapperElement.classList.remove('show')
+
+                  setElementStatus(false)
                 }
               }}>{category ? <p>{category}</p> : <p>Select a category</p>}</button>
               <div className='categoriesDiv'>
-                {categories.map(category => <div className='categoryList' key={category.id} onClick={()=> setCategory(category.category)}>
-                  <h2>{category.category}</h2>
+                <p>Select A Category</p>
+                {
+                categories.map(category => <div className={`categoryList ${category.id}`} key={category.id} onClick={()=> {setCategory(category.category)}}>
+                  <h2 id='categoryHeader' onClick={()=> {
+                   const categoriesElement = document.querySelector('.categoriesDiv')
+                  const wrapperElement = document.querySelector('.wrapper')
+                  setCategory(category.category)
+                  setElementStatus(false)
+                  categoriesElement.classList.remove('show')
+                  wrapperElement.classList.remove('show')
+
+                  
+               
+                  }}>{category.category}</h2>
                 </div>)}
               </div>
               </div>
               <div id='solutionWrapper'>
 
-              {solutions.map(solution => <div id='solution' key={solution.id}>
-                <h1>{solution.category}</h1>
-                <p>{solution.part}</p>
-                <p>{solution.repair_solution}</p>
-                <p>{solution.estimated_cost}</p>
-              </div>)}
+              {
+                <>
+                  { filteredItems.length === 0 ?            
+                    solutions.map(solution => <div id='solution' key={solution.id}>
+                    <h1>{solution.category}</h1>
+                    <p>{solution.part}</p>
+                    <p>{solution.repair_solution}</p>
+                    <p>Average Cost: ${solution.estimated_cost}</p>
+                  </div>)
+                :
+              
+                      filteredItems.map(solution => <div id='solution' key={solution.id}>
+
+                    <h1>{solution.part}</h1>
+                    <p>{solution.repair_solution}</p>
+                    <p>Average Cost: ${solution.estimated_cost}</p>
+
+
+                      </div>)
+              
+                }
+              </>
+              }
                 </div>
 
 
