f163559d90eb4b53d4736ea41cb705f09f57816d
Roland Edney <redney@maets.net>
2025-09-17 14:35:30 -0400

    Sync changes

--- Backend/app.js
+++ Backend/app.js
@@ -1,48 +1,32 @@
 import express from "express";
-import cors from "cors";
-
+import cors from "cors"; // Add this line
 const app = express();
 export default app;
-
-import loginRouter from '../Backend/api/login.js';
-import registerRouter from '../Backend/api/register.js';
-import foraRouter from './api/fora.js';
-import accountRouter from './api/account.js';
-import solutionsRouter from './api/solutions.js';
-
-/* 1) FIRST middleware */
-app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', '*');
-  res.setHeader('Vary', 'Origin');
-  res.setHeader('Access-Control-Allow-Methods', 'GET,POST,PUT,PATCH,DELETE,OPTIONS');
-  res.setHeader('Access-Control-Allow-Headers', 'Content-Type,Authorization');
-  if (req.method === 'OPTIONS') return res.sendStatus(204);
-  next();
-});
-
-const corsForSite = cors({
-  origin: 'https://retekprojects.com',
-  methods: ['GET','POST','PUT','PATCH','DELETE','OPTIONS'],
-  allowedHeaders: ['Content-Type','Authorization'],
-  credentials: false
-});
+import loginRouter from '../Backend/api/login.js'
+import registerRouter from '../Backend/api/register.js'
+import foraRouter from './api/fora.js'
+import accountRouter from './api/account.js'
+import solutionsRouter from './api/solutions.js'
+// Allow all CORS requests -- had issues getting backend and front end to take and send requests
+app.use(cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use("/login", loginRouter)
+app.use("/register", registerRouter)
+app.use("/fora", foraRouter)
+app.use("/account", accountRouter)
+app.use("/solutions", solutionsRouter)
 
-app.use('/login',     corsForSite, loginRouter);
-app.use('/register',  corsForSite, registerRouter);
-app.use('/fora',      corsForSite, foraRouter);
-app.use('/account',   corsForSite, accountRouter);
-app.use('/solutions', corsForSite, solutionsRouter);
-
-/* 4) Error handlers */
 app.use((err, req, res, next) => {
   switch (err.code) {
-    case "22P02": return res.status(400).send(err.message);
+    case "22P02":
+      return res.status(400).send(err.message);
     case "23505":
-    case "23503": return res.status(400).send(err.detail);
-    default: next(err);
+    case "23503":
+      return res.status(400).send(err.detail);
+    default:
+      next(err);
   }
 });
 
